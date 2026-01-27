fbad765f2cdaa0d2ecc636337735f63647472e54
Mimi Okoi <mimiokoiobuli@gmail.com>
2026-01-27 14:42:20 +0300

    Final SRS compliance: Add sitemap and fix press page links

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import netlify from '@astrojs/netlify';
+import sitemap from '@astrojs/sitemap';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: netlify()
+  site: 'https://amiiverse.com',
+  adapter: netlify(),
+  integrations: [sitemap()]
 });
