b4604ee4940a823d48a9eb9781776c9db3062322
Mimi Okoi <mimiokoiobuli@gmail.com>
2026-02-18 15:02:37 +0300

    feat: add deployment configuration to Sanity CLI

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -4,5 +4,9 @@ export default defineCliConfig({
     api: {
         projectId: 'pxn399gi',
         dataset: 'production'
-    }
+    },
+    deployment: {
+    appId: 'cc38d9933uk2lc021vvm1upp',
+    autoUpdates: true
+  }
 })
