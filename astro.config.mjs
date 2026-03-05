aee5b2090126bd4b509c83c12f6fe34a4e56ab64
Mimi Okoi <mimiokoiobuli@gmail.com>
2026-03-05 13:46:47 +0300

    fix: disable embedded sanity studio route for cloudflare pages runtime

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import cloudflare from '@astrojs/cloudflare';
 import sitemap from '@astrojs/sitemap';
 import icon from 'astro-icon';
 import sanity from '@sanity/astro';
-import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,14 +13,12 @@ export default defineConfig({
   }),
   integrations: [
     icon(),
-    react(),
     sitemap(),
     sanity({
       projectId: process.env.PUBLIC_SANITY_PROJECT_ID || 'pxn399gi',
       dataset: process.env.PUBLIC_SANITY_DATASET || 'production',
       useCdn: false, // set to true for production if you want caching
-      studioBasePath: '/admin',
-      studioRouterHistory: 'hash',
+      studioBasePath: null,
     }),
   ],
   image: {
